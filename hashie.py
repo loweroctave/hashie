23bdfb0accc50f58ea417711f984029b118b6f64
Terry <terry@leapservices.com>
2012-11-08 16:04:41 -0600

    Initial version.

---
+++ hashie.py
@@ -0,0 +1,78 @@
+# hashie.py
+# @author Terry Winters (terry@leapservices.com)
+# @internal @created 11-8-2012
+
+class hash(object):
+	
+	def __init__(self, redis, name=None):
+		if redis: self.db = redis
+		if name: self.get(name)
+		
+	def create(self, key, score=None, data=None):
+		self.key = key
+		if score: self.score = score
+		if data: self.data = data
+		
+	# commit a hash to db
+	def commit(self):
+		self.db.hmset(self.key, self.data)
+	
+	# get a hash from the db
+	def get(self, key):
+		self.data = self.db.hgetall(key)
+		return self.data
+	
+	# get value for a key
+	def val(self, key):
+		return self.data.get(key) if self.data.get(key) else None
+	
+class hashset(object):
+	
+	def __init__(self, redis, name=None):
+		if redis: self.db = redis
+		if name: self.new(name)
+		self.hashes = []
+		self.setData = []
+	
+	# load a set with range
+	def getRange(self, r1, r2):
+		self.setData = self.db.zrangebyscore(self.set, r1, r2, withscores=True)
+		self.loadHashes()
+		return self.setData
+	
+	# remove range from redis
+	def removeRange(self, r1, r2):
+		self.db.zremrangebyscore(self.set, r1, r2)
+	
+	# getRange and delete from redis
+	def clearRange(self, r1, r2):
+		self.getRange(r1, r2)
+		self.removeRange(r1, r2)
+		return self.setData
+	
+	# add a hash to set/redis
+	def add(self, hash):
+		self.hashes.append(hash)
+		hash.commit()
+		self.db.zadd(self.set, hash.key, hash.score)
+	
+	# remove a hash from redis
+	def delete(self, hashName):
+		self.db.delete(hashName)
+		
+	def loadHashes(self):
+		for i in self.setData:
+			h = hash(self.db, i[0])
+			self.hashes.append(h)
+		return len(self.hashes)
+	
+	# start a new set
+	def new(self, setName):
+		if setName: self.set = setName
+	
+	# return the hashes
+	def getHashes(self):
+		return self.hashes if self.hashes else []
+		
+	def getHash(self, index):
+		return self.hashes[index] if self.hashes[index] else False
